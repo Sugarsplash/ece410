875eef1ab13ca7a492e46d7ab3664b0a4a34567f
Sugarsplash <bryan3@pdx.edu>
2016-06-08 13:20:11 -0700

    Add buy and sell points to plot, add buy and sell values, attempt to qualify data with research paper.

--- src/svm.py
+++ src/svm.py
@@ -18,30 +18,71 @@ date_stop = date.datetime(2011, 6, 20)
 hist = pandas.DataReader(SYMBOL, 'yahoo', date_start, date_stop)
 hist_test = np.array(hist['Adj Close'].tolist())
 
-sma_6 = ta.SMA(hist_train, timeperiod=6)
-sma_65 = ta.SMA(hist_train, timeperiod=65)
+sma_5 = ta.SMA(hist_train, timeperiod=5)
+sma_60 = ta.SMA(hist_train, timeperiod=60)
 
+#From Research Paper:
+#Buying rule: 5-simple moving average crosses up 60-simple moving average
+#Selling rule: 5-simple moving average crosses down 60-simple moving average
 buy_day = []
+buy_value = []
 sell_day = []
+sell_value = []
 crossover = False
 for i in range(len(hist_train)):
-    if ( (sma_6[i] > sma_65[i]) and
+    if ( (sma_5[i] > sma_60[i]) and
          (crossover == False) ):
 
+        #need to skip first crossover
         buy_day.append(i)
-        print("Long: Day {0}".format(i))
+        buy_value.append(hist_train[i])
+        print "Long: Day " + str(i) + " Value = " + str(hist_train[i])
         crossover = True
 
-    elif ( (sma_6[i] < sma_65[i]) and
+    elif ( (sma_5[i] < sma_60[i]) and
            (crossover == True) ):
 
         sell_day.append(i)
-        print("Exit: Day {0}".format(i))
+        sell_value.append(hist_train[i])
+        print "Exit: Day " + str(i) + " Value = " + str(hist_train[i])
         crossover = False
 
+
+#collect data about results to compare to table 2 of research paper
+#need Sum and avg of return, earning(,) and loss...(oxford comma or no?)
+#need success rate and others but fuck it.
+trades = []
+success = 0
+fail = 0
+
+for buy, sell in zip(buy_value, sell_value): 
+    trades.append(sell - buy)
+
+for result in trades:
+    print "Trade: " + str(result)
+    if result > 0:
+        success = success + 1
+    else:
+        fail = fail + 1
+
+winRate = float(success)/float(success + fail)
+
+#sanity check:
+#print "Win: " + str(success) + " Lose: " + str(fail) + " Total: " + str(len(trades))
+#print str(winRate)
+
+
+
+
+
+#Lets paint happy figures
 #plt.plot(hist_train, 'r-', label='Raw')
-plt.plot(sma_6, 'g-', label='SMA = 6')
-plt.plot(sma_65, 'b-', label='SMA = 65')
+plt.plot(sma_5, 'g-', label='SMA = 5')
+plt.plot(sma_60, 'b-', label='SMA = 60')
+for each in buy_day:
+    plt.plot(each, hist_train[each], 'ro')
+for item in sell_day:
+    plt.plot(item, hist_train[item], 'bo')
 plt.xticks(range(0, len(hist_train), 100))
 plt.legend(loc='upper left')
 plt.xlabel("Trading Days")
